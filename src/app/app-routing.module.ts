72830108cf2a47a07ab05387f37cfd4931e2a9a0
Mateusz Chmielewski <chmielewski079@gmail.com>
2019-11-18 19:44:40 +0100

    added game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostComponent } from './components/post-components/post/post.component';
 import { SinglePostComponent } from './components/post-components/single-post/single-post.component';
+import { BlackjackComponent } from './components/game/blackjack/blackjack.component';
+
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 
@@ -17,6 +19,10 @@ const routes: Routes = [
   {
     path:'register',
     component: RegisterPageComponent
+  },
+  {
+    path:'game',
+    component: BlackjackComponent
   }
 ];
 
